127d3acc03a320cec082b2bba65f9591364a83fa
Noah Krossa <noahkrossa@gmail.com>
2021-01-07 11:50:20 +0100

    Connect store to React app

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AppComponent from './app'
-ReactDOM.render(<AppComponent />, document.querySelector('#root'))
+import Store from './store/index'
+import { Provider } from 'react-redux'
+
+ReactDOM.render(
+  <Provider store={Store}>
+    <AppComponent />
+  </Provider>,
+  document.querySelector('#root')
+)
