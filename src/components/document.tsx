b7d8886a72a3f5425dc0729f548699112f1aae42
Noah Krossa <noahkrossa@gmail.com>
2021-01-08 12:14:00 +0100

    Set mapStateToProps as a object & add styles base

--- src/components/document.tsx
+++ src/components/document.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { generateNodeComponentHOC } from './node'
+import { generateNodeComponentHOC, ContentComponent } from './node'
 import { connect, ConnectedProps } from 'react-redux'
-import { RootState } from '../store'
-import { DocumentNode, DocumentState } from '../store/types'
+import { AppDispatch, RootState } from '../store'
+import { DocumentState } from '../store/types'
 import styles from './document.m.scss'
+import {
+  documentAPIRequestAction,
+  fetchDocumentSuccessAction,
+  removeDocumentNodeAction,
+  updateDocumentNodeAction
+} from '../store/actions'
 
 const mapStateToProps = (state: RootState): DocumentState => {
   const { nodeList, loading, error } = state.document
@@ -13,14 +19,18 @@ const mapStateToProps = (state: RootState): DocumentState => {
     error
   }
 }
-const mapStateToDispatch = (dispatch: any) => {
-  return {
-    fetchNodes: () => {},
-    removeNode: (id: string) => {},
-    updtateNode: (node: DocumentNode) => {},
-    addNode: (node: DocumentNode) => {}
-  }
+const mapStateToDispatch = {
+  fetchNodes: () => async (dispatch: AppDispatch) => {
+    dispatch(documentAPIRequestAction())
+    await dispatch(
+      fetchDocumentSuccessAction([{ id: '123', content: '<h2>sdfsfs</h2>' }])
+    )
+  },
+  updateNode: () => async (dispatch: AppDispatch) => {},
+  removeNode: () => async (dispatch: AppDispatch) => {},
+  addNode: () => async (dispatch: AppDispatch) => {}
 }
+
 const connector = connect(mapStateToProps, mapStateToDispatch)
 type PropsFromRedux = ConnectedProps<typeof connector>
 type Props = PropsFromRedux & {
@@ -33,14 +43,20 @@ class DocumentComponent extends React.Component<Props> {
     super(props)
   }
   render() {
+    const { nodeList, updateNode } = this.props
+
     return (
       <div className={styles.document}>
-        {/* {nodeList.map((node) => {
+        {nodeList.map((node) => {
           const Node = generateNodeComponentHOC(
-            <ContentComponent HTMLContent={node.content} />
+            <ContentComponent
+              HTMLContent={node.content}
+              id={node.id}
+              update={updateNode}
+            />
           )
           return <Node key={node.id} />
-        })} */}
+        })}
       </div>
     )
   }
